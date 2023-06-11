0c19117e70676d4c24b3cac6c87b72e6b767705f
minu21shahabas <minu27theparambil@gmail.com>
2023-06-10 22:42:18 -0700

    first commit

---
+++ proapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'frontview.html')
+def login(request):
+    return render(request,'login.html')
+def sign(request):
+    return render(request,'signup.html')
+def homes(request):
+    return render(request,'home.html')
+def con(request):
+    return render(request,'contact.html')
